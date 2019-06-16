23dd2bef42bc5c3c394891455602e1000f9877c4
Philip R. Jarnhus <jarnhus@gmail.com>
2019-06-16 20:50:35 +0200

    Add function for outputting list of all topics
    It quickly becomes difficult to remember which topics are available.
    This function remedies this.
    
    The function simply returns a list of all topics with no possibility to
    filter in any way.
    
    Scope: Database

--- io_funcs.py
+++ io_funcs.py
@@ -12,6 +12,17 @@ def find_topic(topic_name: str):
     return [r for r in rows]
 
 
+def get_all_topics():
+    sql_query = 'select tag_name from tag_md order by tag_name'
+    rows = (db
+            .conn
+            .cursor()
+            .execute(sql_query)
+            .fetchall()
+           )
+    return [r[0] for r in rows]
+
+
 def create_topic(topic_name: str):
     sql_query = f"insert into tag_md(tag_name) values ('{topic_name}')"
     cur = db.conn.cursor()
