daa9c963bb3f3ee8caa00016bc1ef9e0977168e2
Philip R. Jarnhus <jarnhus@gmail.com>
2019-06-16 20:46:31 +0200

    Standardised topic input
    Created a standard function for all inputs of topics. This enables the
    user to list all topics in a uniform way.
    
    Should probably be generalised more to encompass other types of entries
    as well.
    
    Scope: UI

--- ui.py
+++ ui.py
@@ -10,10 +10,21 @@ def print_status(msg: str):
     print()
 
 
+def input_topic(text:str):
+    while True:
+        print('-  Press ? to list all topics  -')
+        topic_name = input(text)
+        if topic_name == '?':
+            print('-  Topics available  -')
+            print('\n'.join(io_funcs.get_all_topics()))
+            print()
+            continue
+        return topic_name
+
 def add_topic():
     print_status('Add new topic')
     while True:
-        topic_name = input('Topic (empty to abort): ')
+        topic_name = input_topic('Topic (empty to abort): ')
 
         if len(topic_name) == 0:
             return "No new topic was added"
@@ -27,11 +38,11 @@ def add_topic():
 def add_tag():
     print_status('Add new tag to topic')
     while True:
-        topic_name = input('Topic that should be tagged: ')
+        topic_name = input_topic('Topic that should be tagged: ')
         topic = io_funcs.find_topic(topic_name)
         if not topic:
             return 'Topic does not exist'
-        tag_name = input('Tag name (empty to abort): ')
+        tag_name = input_topic('Tag name (empty to abort): ')
         if len(tag_name) == 0:
             return 'No tag added'
 
