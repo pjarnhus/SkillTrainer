ff54bf9a07d48ec01dca6f7afff386d5d4778e79
Philip R. Jarnhus <jarnhus@gmail.com>
2019-06-16 20:23:54 +0200

    Add I/O operation for creating new tags for topics
    Write the tag to a topic in the database.
    
    There is no check for cyclic behaviour and no way to create topics on
    the fly.
    
    Scope: Database

--- io_funcs.py
+++ io_funcs.py
@@ -2,11 +2,12 @@ import db
 
 
 def find_topic(topic_name: str):
-    sql_query = f"select tag_name from tag_md where tag_name = '{topic_name}'"
+    sql_query = f"select tag_id, tag_name from tag_md where tag_name = '{topic_name}'"
     rows = (db
             .conn
             .cursor()
             .execute(sql_query)
+            .fetchall()
            )
     return [r for r in rows]
 
@@ -17,3 +18,23 @@ def create_topic(topic_name: str):
     cur.execute(sql_query)
     db.conn.commit()
     return f"Created topic: {topic_name}"
+
+
+def create_tag(topic, tag):
+    sql_test_exists = (f"select * from tags where child_id = {topic[0]}"
+                       + f" and parent_id = {tag[0]}")
+    rows = (db
+            .conn
+            .cursor()
+            .execute(sql_test_exists)
+            .fetchall()
+           )
+    if rows:
+        return f'{topic[1]} is already tagged with {tag[1]}'
+
+    sql_create_tag = (f"insert into tags(child_id, parent_id)"
+                      + f" values ({topic[0]}, {tag[0]})")
+    cur = db.conn.cursor()
+    cur.execute(sql_create_tag)
+    db.conn.commit()
+    return f'{topic[1]} tagged under {tag[1]}'
