c94e35d7770d81e8d39ab61032a53a683e9aea31
Philip R. Jarnhus <jarnhus@gmail.com>
2019-06-17 12:12:10 +0200

    Create simple Web Item class
    In order to assist the user in adding items, we create a web item class
    for retrieving title and topic suggestions. This should speed up the
    addition of new items.
    
    The implementation is very rudimentary. There is no visual feedback for
    the webpage being scraped. The topic suggestion can be very prone to
    false positives.
    
    Scope: Web

---
+++ web.py
@@ -0,0 +1,36 @@
+import bs4
+import io_funcs
+import re
+import requests
+
+class WebItem:
+    def __init__(self, url):
+        self.url = url
+        try:
+            self.webpage = requests.get(self.url, timeout = 5)
+            if self.webpage.status_code == 200:
+                self.succeeded = True
+                self.content = bs4.BeautifulSoup(self.webpage.content,
+                                                 'html.parser')
+            else:
+                self.succeeded = False
+        except:
+            self.succeeded = False
+
+
+    def get_title(self):
+        if not self.succeeded:
+            return None
+        return self.content.find('title').text
+
+
+    def get_suggested_topics(self, n_max=10):
+        topics = io_funcs.get_all_topics()
+        body = self.content.find('body').text
+        word_pattern = re.compile(r'\w+')
+        unique_words = set(word_pattern.findall(body.lower()))
+        found_topics = [topic for topic in topics
+                        if all([topic_word.lower() in unique_words
+                                for topic_word in
+                                word_pattern.findall(topic)])]
+        return found_topics[:n_max]
