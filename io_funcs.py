9ce6f8dada2a76349052764baf42aa366074a42e
Philip R. Jarnhus <jarnhus@gmail.com>
2019-06-17 20:37:52 +0200

    Add function to write new item to library
    This function actually writes the item to the database, along with
    topic, title and relevant levels.
    
    The current implementation only allows for level ranges.
    
    Scope: Database

--- io_funcs.py
+++ io_funcs.py
@@ -23,6 +23,17 @@ def get_all_topics():
     return [r[0] for r in rows]
 
 
+def get_all_levels():
+    sql_query = 'select level_id, level_name from levels'
+    rows = (db
+            .conn
+            .cursor()
+            .execute(sql_query)
+            .fetchall()
+           )
+    return rows
+
+
 def create_topic(topic_name: str):
     sql_query = f"insert into tag_md(tag_name) values ('{topic_name}')"
     cur = db.conn.cursor()
@@ -49,3 +60,27 @@ def create_tag(topic, tag):
     cur.execute(sql_create_tag)
     db.conn.commit()
     return f'{topic[1]} tagged under {tag[1]}'
+
+
+def create_item(title,
+                reference,
+                topic = None,
+                low_level = None,
+                high_level = None):
+    if topic:
+        sql_query = ('insert into library(title, reference,'
+                     + ' tag_id, level_low, level_high)'
+                     + ' values('
+                     + f"'{title}', '{reference}', {topic[0][0]},"
+                     + f" {low_level}, {high_level})"
+                    )
+        return_string = f'{title} was inserted under {topic[0][1]}'
+    else:
+        sql_query = ('insert into library(title, reference)'
+                     + f"values ('{title}', '{reference}')"
+                    )
+        return_string = f'{title} was inserted in To Read'
+    cur = db.conn.cursor()
+    cur.execute(sql_query)
+    db.conn.commit()
+    return return_string
